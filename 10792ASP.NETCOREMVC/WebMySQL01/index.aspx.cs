42ac1e0562998f8dc6c79ad572ea49841be5a68c
Maria <conceicao.martinho.estudo@gmail.com>
2024-01-31 14:34:34 +0000

    alkterações

---
+++ 10792ASP.NETCOREMVC/WebMySQL01/index.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebMySQL01
+{
+    public partial class index : System.Web.UI.Page
+    {
+        DBconnect ligacao = new DBconnect();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblNFormandos.Text = ligacao.Count().ToString();
+            lblMedia.Text=ligacao.Media().ToString("0.00");
+
+            if(!Page.IsPostBack)
+            {
+                ligacao.Bind(ref GridView1);
+                GridView1.DataBind();
+            }
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebFormInsert.aspx");
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("WebFormDelete.aspx");
+        }
+
+        protected void GridView1_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            ligacao.Bind(ref GridView1);
+            GridView1.DataBind();
+        }
+    }
+}
